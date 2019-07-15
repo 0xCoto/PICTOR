c24e45d7b1907e937bcada474f2a0342535e9730
0xCoto <cotothearcher@gmail.com>
2019-07-15 23:41:34 +0300

    Update observe.py

--- observe.py
+++ observe.py
@@ -59,13 +59,13 @@ while True:
         #Send plot to observer's email
         fromaddr = 'pictortelescope@gmail.com'
         toaddr = email
-
+        
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-
+        
         msg['Subject'] = '['+id+'] Observation Data'
-
+        
         body = '''Your observation has been carried out by PICTOR successfully!
 Observation name: '''+obs_name+'''
 Observation datetime: '''+obsDT+''' (UTC+3)
@@ -77,25 +77,25 @@ Number of bins: '''+nbins+'''
 Observation duration: '''+duration+''' sec
 Observation ID: '''+id+'''
 Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs Time plot are attached in this email as an image.'''
-
+        
         msg.attach(MIMEText(body, 'plain'))
-
+        
         filename = 'plot.png'
         attachment = open("/home/pictor/Desktop/pictortelescope/"+filename, 'rb')
-
+        
         p = MIMEBase('application', 'octet-stream')
         p.set_payload((attachment).read())
-
+        
         encoders.encode_base64(p)
         p.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
         msg.attach(p)
-
+        
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(fromaddr, 'XXX') #XXX: plaintext email password
-
+        
         text = msg.as_string()
-
+        
         s.sendmail(fromaddr, toaddr, text)
         s.quit()
         
