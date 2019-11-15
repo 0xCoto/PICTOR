fd2064b6175b1d99959aab279c612eb40d00e26b
Yannick <40766244+YannickDC@users.noreply.github.com>
2019-11-15 17:35:48 +0100

    Updating PHP code (#4)

--- observe.php
+++ observe.php
@@ -1,20 +1,49 @@
-<!DOCTYPE html>
+<?php
+$pictorIsDown = false;
+foreach (json_decode(file_get_contents("https://discordapp.com/api/guilds/644685390116290560/widget.json"), true)['members'] as $user) {
+    if ($user["username"] == 'PICTOR') {
+        $pictorIsDown = true;
+    }
+}
+$statusFile = fopen("checkStatus.txt", "w") or die("Unable to open file!");
+if($pictorIsDown){
+    fwrite($statusFile,"true");
+
+}else{
+    fwrite($statusFile,"false"); // pictor down
+}
+
+fclose($statusFile);
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>PICTOR: Telescope Control</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
 	<div class="container-contact100">
@@ -23,6 +52,7 @@
 				<span class="contact100-form-title">
 					PICTOR Telescope Control
 					<hr>
+				<center><font color="1E90FF" size="4"><b>&#9432; New to radio astronomy?</b></font><font color="1E90FF" size="3"><br>Click <b><u><a href="Observing_the_radio_sky_with_PICTOR.pdf" target="_blank"><font color="7BB33A" size="3">here</font></a></u></b> to learn how to use <b>PICTOR</b> and observe the radio sky!</font></center>
 				</span>
 
 
@@ -34,7 +64,7 @@
 
 				<div class="wrap-input100 validate-input" data-validate="Center frequency is required">
 					<label class="label-input100" for="f_center">Center frequency (MHz)</label>
-					<input id="f_center" class="input100" type="number" name="f_center" min="1300" max="1700" placeholder="Enter the center frequency of your observation..." required>
+					<input id="f_center" class="input100" type="number" name="f_center" min="1300" max="1700" placeholder="Enter the center frequency of your observation..." value="1420" required>
 					<span class="focus-input100"></span>
 				</div>
 
@@ -59,8 +89,8 @@
 						<select class="js-select2" name="channels">
 							<option value="256" >256</option>
 							<option value="512" >512</option>
-							<option value="1024"  selected="selected">1024</option>
-							<option value="2048"  >2048</option>
+							<option value="1024">1024</option>
+							<option value="2048" selected="selected">2048</option>
 						</select>
 						<div class="dropDownSelect2"></div>
 					</div>
@@ -70,13 +100,14 @@
 
                 <div class="wrap-input100 validate-input" data-validate="Number of bins is required">
                     <label class="label-input100" for="nbins">Number of bins</label>
-                    <input id="nbins" class="input100" type="number" name="nbins" min="100" max="100000" placeholder="Enter the number of bins..." required>
+					<input id="nbins" class="input100" type="number" name="nbins" min="1000" max="20000" placeholder="Enter the number of bins..." value="1000" required="">
                     <span class="focus-input100"></span>
                 </div>
 
 				<div class="wrap-input100 validate-input" data-validate="Observing duration is required">
 					<label class="label-input100" for="f_center">Duration (sec)</label>
-                    <input id="duration" class="input100" type="number" name="duration" min="10" max="36000" placeholder="Enter the duration of your observation..." required>
+                    <input id="duration" class="input100" type="number" name="duration" min="10" max="3600" placeholder="Enter the duration of your observation..." required="">
+
 					<span class="focus-input100"></span>
 				</div>
 <hr>
@@ -92,11 +123,15 @@
                         Submit
 					</button>
 				</div>
-				<div id="currentlyInUseMessage" style="display:none; color: red">
+				<div id="currentlyInUseMessage" style="display:none; color: orangered">
 					The telescope is currently observing. Please wait until it's done before submitting your observation!
 				</div>
+                <div id="currentlyOffline" style="display:none; color: red">
+                    The telescope is currently under maintenance. Please check back in a few hours!
+                </div>
 				<hr>
-				PICTOR is a free-to-use open source radio telescope that allows anyone to make continuous and spectral drift-scan observations of the radio sky in the 1300~1700 MHz regime.
+				<b>PICTOR</b>, located in <b>Athens, Greece</b>, is a free-to-use open source radio telescope that allows anyone to make continuous and spectral drift-scan observations of the radio sky in the <b>1300~1700 MHz</b> regime.
+
         <br><br>
 				<p style="font-size:95%;">Contact: <a style="font-size:95%;" href="mailto:0xcoto@protonmail.com">0xcoto@protonmail.com</a></p>
 				<p style="font-size:95%;">GitHub: <a style="font-size:95%;" href="https://github.com/0xCoto/PICTOR" target="_blank">https://github.com/0xCoto/PICTOR</a></p>
@@ -111,10 +146,14 @@
 
 
 
+<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
 	<script>
 		$(".js-select2").each(function(){
@@ -135,24 +174,49 @@
 		});
 
 	</script>
+<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 
 <script>
 
+
 $(function() {
     getLastObservedFile(parseLastObservedFileAndUpdateUi);
+    getStatusFile(parseStatus);
 });
 
 function getLastObservedFile(callback)
 {
-    $.get("/last_obs.txt", function(data) { callback(data); } );
+    $.get("/last_obs_duration.txt", function(data) { callback(data); } );
+}
+function getStatusFile(callback)
+{
+    $.get("/checkStatus.txt", function(data) { callback(data); } );
+}
+function parseStatus(fileContents) {
+   var status = fileContents
+
+    if (status == "true") {
+        $("#currentlyOffline").hide();
+        $("#currentlyInUseMessage").hide();
+        $(".contact100-form-btn").show();
+    }
+    else if (status == "false") {
+        $(".contact100-form-btn").hide();
+        $("#currentlyInUseMessage").hide();
+        $("#currentlyOffline").show();
+        return;
+    }
 }
 function parseLastObservedFileAndUpdateUi(fileContents) {
     var lines = fileContents.split("\n");
+
 
     var durationLine = lines[5];
     var durationInSeconds = parseInt(durationLine.split("=")[1].replace("'", ""));
@@ -163,15 +227,17 @@ function parseLastObservedFileAndUpdateUi(fileContents) {
     var currentTimeUnix = Math.round((new Date()).getTime() / 1000);
     var timeToWait = startTimeInSeconds - currentTimeUnix + durationInSeconds;
 
-	$(".contact100-form-btn").hide();
-	$("#currentlyInUseMessage").show();
+    $(".contact100-form-btn").hide();
+    $("#currentlyInUseMessage").show();
 
     setTimeout(
-      function()
-      {
-        $(".contact100-form-btn").show();
-		$("#currentlyInUseMessage").hide();
-      }, timeToWait * 1000);
+        function()
+        {
+            if (online) {
+                $(".contact100-form-btn").show();
+                $("#currentlyInUseMessage").hide();
+            }
+        }, timeToWait * 1000);
 }
 
 </script>
@@ -182,22 +248,32 @@ if(isset($_REQUEST['submit_btn']))
 {
 
 
-    //Getting all the values
-    $date = new DateTime();
+   // getting all the value's
+	$date = new DateTime();
     $obs_name = $_POST["obs_name"];
     $f_center= $_POST["f_center"];
     $bandwidth = $_POST["bandwidth"];
-    $obs_datetime = $_POST["obs_datetime"];
+ 
     $channels = $_POST["channels"];
     $nbins = $_POST["nbins"];
     $duration = $_POST["duration"];
     $email = $_POST["email"];
     $check = random_int(10000000, 99999999);
     $time = $date->getTimestamp();
+    // Writing away
+	$writeObs = fopen("last_obs_duration.txt", "w") or die("Unable to open file!");
+    fwrite($writeObs, "obs_name="."'"."0"."'"."\n");
+    fwrite($writeObs, "f_center="."'"."0"."'"."\n");
+    fwrite($writeObs, "bandwidth="."'"."0"."'"."\n");
+    fwrite($writeObs, "channels="."'"."0"."'"."\n");
+    fwrite($writeObs, "nbins="."'"."0"."'"."\n");
+    fwrite($writeObs, "duration="."'".$duration."'"."\n");
+    fwrite($writeObs, "id="."'"."0"."'"."\n");
+    fwrite($writeObs, "obs_time="."'". $time."'"."\n");
+	fclose($writeObs);	
 
-    //Writing away
+
     $myfile = fopen("last_obs.txt", "w") or die("Unable to open file!");
-
     fwrite($myfile, "obs_name="."'".$obs_name."'"."\n");
     fwrite($myfile, "f_center="."'".$f_center."'"."\n");
     fwrite($myfile, "bandwidth="."'".$bandwidth."'"."\n");
@@ -207,6 +283,10 @@ if(isset($_REQUEST['submit_btn']))
     fwrite($myfile, "email="."'".$email."'"."\n");
     fwrite($myfile, "id="."'".$check."'"."\n");
     fwrite($myfile, "obs_time="."'". $time."'"."\n");
+
+	
+
+
 
 
 
