548943fba7b9ba7dafac074f29395f1055342de9
ministerofsillywalks <70113831+ministerofsillywalks@users.noreply.github.com>
2020-08-25 19:58:56 -0400

    Update observe.py to send raw data
    if asked for by observe.php

--- observe.py
+++ observe.py
@@ -40,10 +40,12 @@ while True:
     elif bandwidth == '3.2mhz':
         bandwidth = '3200000'
     
-    #Delete pre-existing observation.dat & plot.png files
+    #Delete pre-existing observation.dat, plot.png, data_freq.csv, & data_time.csv files
     try:
         os.remove('/home/pictor/Desktop/pictortelescope/observation.dat')
         os.remove('/home/pictor/Desktop/pictortelescope/plot.png')
+        os.remove('/home/pictor/Desktop/pictortelescope/data_freq.csv')
+        os.remove('/home/pictor/Desktop/pictortelescope/data_time.csv')
     except OSError:
         pass
     
@@ -71,7 +73,7 @@ while True:
             copy2('/home/pictor/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off1024.dat')
         elif obs_name == 'off2048':
             copy2('/home/pictor/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off2048.dat')
-        
+            
         #Send plot to observer's email
         fromaddr = 'pictortelescope@gmail.com'
         toaddr = email
@@ -83,7 +85,6 @@ while True:
         msg['Subject'] = '['+id+'] Observation Data'
         
         body = '''Your observation has been carried out by PICTOR successfully!
-
 Observation name: '''+obs_name+'''
 Observation datetime: '''+obsDT+''' (UTC+3)
 Center frequency: '''+f_center+''' Hz
@@ -93,21 +94,53 @@ Number of channels: '''+channels+'''
 Number of bins: '''+nbins+'''
 Observation duration: '''+duration+''' sec
 Observation ID: '''+id+'''
-
 Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs Time plot are attached in this email as an image.'''
         
         msg.attach(MIMEText(body, 'plain'))
         
-        filename = 'plot.png'
-        attachment = open("/home/pictor/Desktop/pictortelescope/"+filename, 'rb')
+        filename1 = 'plot.png'
+        attachment1 = open("/home/pictor/Desktop/pictortelescope/"+filename1, 'rb')
         
-        p = MIMEBase('application', 'octet-stream')
-        p.set_payload((attachment).read())
+        p1 = MIMEBase('application', 'octet-stream')
+        p1.set_payload((attachment1).read())
         
-        encoders.encode_base64(p)
-        p.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
-        msg.attach(p)
-        
+        encoders.encode_base64(p1)
+        p1.add_header('Content-Disposition', 'attachment; filename= %s' % filename1)
+        msg.attach(p1)
+        if (raw_data == '1'):
+            #Add frequency vs. Power Data
+            filename2 = 'data_freq.csv'
+            attachment2 = open("/home/pictor/Desktop/pictortelescope/"+filename2, 'rb')
+
+            p2 = MIMEBase('application', 'octet-stream')
+            p2.set_payload((attachment2).read())
+
+            encoders.encode_base64(p2)
+            p2.add_header('Content-Disposition', 'attachment; filename= %s' % filename2)
+            msg.attach(p2)
+
+            #Add Time vs. Power Data
+            filename3 = 'data_time.csv'
+            attachment3 = open("/home/pictor/Desktop/pictortelescope/"+filename3, 'rb')
+
+            p3 = MIMEBase('application', 'octet-stream')
+            p3.set_payload((attachment3).read())
+
+            encoders.encode_base64(p3)
+            p3.add_header('Content-Disposition', 'attachment; filename= %s' % filename3)
+            msg.attach(p3)
+
+            #Add .dat data
+            filename4 = 'observation.dat'
+            attachment4 = open("/home/pictor/Desktop/pictortelescope/"+filename4, 'rb')
+
+            p4 = MIMEBase('application', 'octet-stream')
+            p4.set_payload((attachment4).read())
+
+            encoders.encode_base64(p4)
+            p4.add_header('Content-Disposition', 'attachment; filename= %s' % filename4)
+            msg.attach(p4)
+            
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(fromaddr, 'XXX') #plaintext email password
@@ -128,7 +161,6 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
         msg['Subject'] = '['+id+'] Raw Observation Data'
         
         body = '''Your observation has been carried out by PICTOR successfully!
-
 Observer email: '''+email+'''
 Observation name: '''+obs_name+'''
 Observation datetime: '''+obsDT+''' (UTC+3)
@@ -139,20 +171,41 @@ Number of channels: '''+channels+'''
 Number of bins: '''+nbins+'''
 Observation duration: '''+duration+''' sec
 Observation ID: '''+id+'''
-
 Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs Time plot are attached in this email as an image.'''
         
         msg.attach(MIMEText(body, 'plain'))
         
-        filename = 'observation.dat'
-        attachment = open("/home/pictor/Desktop/pictortelescope/"+filename, 'rb')
+        filename1 = 'observation.dat'
+        attachment1 = open("/home/pictor/Desktop/pictortelescope/"+filename1, 'rb')
         
-        p = MIMEBase('application', 'octet-stream')
-        p.set_payload((attachment).read())
+        p1 = MIMEBase('application', 'octet-stream')
+        p1.set_payload((attachment).read())
         
-        encoders.encode_base64(p)
-        p.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
-        msg.attach(p)
+        encoders.encode_base64(p1)
+        p1.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
+        msg.attach(p1)
+
+        #Add frequency vs. Power Data
+        filename2 = 'data_freq.csv'
+        attachment2 = open("/home/pictor/Desktop/pictortelescope/"+filename2, 'rb')
+
+        p2 = MIMEBase('application', 'octet-stream')
+        p2.set_payload((attachment2).read())
+
+        encoders.encode_base64(p2)
+        p2.add_header('Content-Disposition', 'attachment; filename= %s' % filename2)
+        msg.attach(p2)
+
+        #Add Time vs. Power Data
+        filename3 = 'data_time.csv'
+        attachment3 = open("/home/pictor/Desktop/pictortelescope/"+filename3, 'rb')
+
+        p3 = MIMEBase('application', 'octet-stream')
+        p3.set_payload((attachment2).read())
+
+        encoders.encode_base64(p3)
+        p3.add_header('Content-Disposition', 'attachment; filename= %s' % filename3)
+        msg.attach(p3)
         
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
