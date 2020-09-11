6be519059f616eb005ca7aafc606c183ab6afc40
ministerofsillywalks <70113831+ministerofsillywalks@users.noreply.github.com>
2020-09-11 07:06:38 -0400

    Update observe.py

--- observe.py
+++ observe.py
@@ -18,11 +18,12 @@ while True:
             response = requests.get('https://pictortelescope.com/last_obs.txt', auth=('XXX', 'XXX')) #plaintext credentials
             if response.status_code == 200:
                 exec(response.content)
-                if id not in open('/home/pictor/Desktop/pictortelescope/id_history.txt').read() and 'notpictor' not in obs_name:
+                if id not in open('/home/pi/Desktop/pictortelescope/id_history.txt').read() and 'notpictor' not in obs_name:
                     break
             else:
                 print(response.status_code)
         except Exception as e:
+            print('pictor error on observe.py')
             print(e)
             pass
     
@@ -42,10 +43,10 @@ while True:
     
     #Delete pre-existing observation.dat, plot.png, data_freq.csv, & data_time.csv files
     try:
-        os.remove('/home/pictor/Desktop/pictortelescope/observation.dat')
-        os.remove('/home/pictor/Desktop/pictortelescope/plot.png')
-        os.remove('/home/pictor/Desktop/pictortelescope/data_freq.csv')
-        os.remove('/home/pictor/Desktop/pictortelescope/data_time.csv')
+        os.remove('/home/pi/Desktop/pictortelescope/observation.dat')
+        os.remove('/home/pi/Desktop/pictortelescope/plot.png')
+        os.remove('/home/pi/Desktop/pictortelescope/data_freq.csv')
+        os.remove('/home/pi/Desktop/pictortelescope/data_time.csv')
     except OSError:
         pass
     
@@ -53,26 +54,26 @@ while True:
         #Note current datetime
         currentDT = datetime.datetime.now()
         obsDT = currentDT.strftime("%Y-%m-%d %H:%M:%S")
-        
+        obsDTfile = currentDT.strftime("%Y%m%d%H%M%S")
         #Execute top_block.py with parameters
         sys.argv = ['top_block.py', '--c-freq='+f_center, '--samp-rate='+bandwidth, '--nchan='+channels, '--nbin='+nbins, '--obs-time='+duration]
-        execfile('/home/pictor/Desktop/pictortelescope/top_block.py')
-        
+        execfile('/home/pi/Desktop/pictortelescope/top_block.py')
+        copy2('/home/pi/Desktop/pictortelescope/observation.dat', '/home/pi/Desktop/pictortelescope/obs_archive/'+str(obsDTfile)+'.dat')
         #Execute plotter
         if f_center == '1420000000.0' and bandwidth == '2400000' and (channels == '1024' or channels == '2048'):
             sys.argv = ['plot_hi.py', 'freq='+f_center, 'samp_rate='+bandwidth, 'nchan='+channels, 'nbin='+nbins]
-            execfile('/home/pictor/Desktop/pictortelescope/plot_hi.py')
+            execfile('/home/pi/Desktop/pictortelescope/plot_hi.py')
         else:
             sys.argv = ['plot.py', 'freq='+f_center, 'samp_rate='+bandwidth, 'nchan='+channels, 'nbin='+nbins]
-            execfile('/home/pictor/Desktop/pictortelescope/plot.py')
+            execfile('/home/pi/Desktop/pictortelescope/plot.py')
         
-        with open('/home/pictor/Desktop/pictortelescope/id_history.txt', 'a') as id_logfile:
+        with open('/home/pi/Desktop/pictortelescope/id_history.txt', 'a') as id_logfile:
             id_logfile.write(id+'\n')
         
         if obs_name == 'off1024':
-            copy2('/home/pictor/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off1024.dat')
+            copy2('/home/pi/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off1024.dat')
         elif obs_name == 'off2048':
-            copy2('/home/pictor/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off2048.dat')
+            copy2('/home/pi/Desktop/pictortelescope/observation.dat', '/home/pictor/Desktop/pictortelescope/off2048.dat')
         
         #Send plot to observer's email
         fromaddr = 'pictortelescope@gmail.com'
@@ -99,7 +100,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
         msg.attach(MIMEText(body, 'plain'))
         
         filename1 = 'plot.png'
-        attachment1 = open("/home/pictor/Desktop/pictortelescope/"+filename1, 'rb')
+        attachment1 = open("/home/pi/Desktop/pictortelescope/"+filename1, 'rb')
         
         p1 = MIMEBase('application', 'octet-stream')
         p1.set_payload((attachment1).read())
@@ -110,7 +111,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
         if (raw_data == '1'):
             #Add spectrum Data
             filename2 = 'data_spectrum.csv'
-            attachment2 = open("/home/pictor/Desktop/pictortelescope/"+filename2, 'rb')
+            attachment2 = open("/home/pi/Desktop/pictortelescope/"+filename2, 'rb')
 
             p2 = MIMEBase('application', 'octet-stream')
             p2.set_payload((attachment2).read())
@@ -121,7 +122,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
 
             #Add Time vs. Power Data
             filename3 = 'data_time_power.csv'
-            attachment3 = open("/home/pictor/Desktop/pictortelescope/"+filename3, 'rb')
+            attachment3 = open("/home/pi/Desktop/pictortelescope/"+filename3, 'rb')
 
             p3 = MIMEBase('application', 'octet-stream')
             p3.set_payload((attachment3).read())
@@ -132,7 +133,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
 
             #Add .dat data
             filename4 = 'observation.dat'
-            attachment4 = open("/home/pictor/Desktop/pictortelescope/"+filename4, 'rb')
+            attachment4 = open("/home/pi/Desktop/pictortelescope/"+filename4, 'rb')
 
             p4 = MIMEBase('application', 'octet-stream')
             p4.set_payload((attachment4).read())
@@ -176,7 +177,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
         msg.attach(MIMEText(body, 'plain'))
         
         filename1 = 'observation.dat'
-        attachment1 = open("/home/pictor/Desktop/pictortelescope/"+filename1, 'rb')
+        attachment1 = open("/home/pi/Desktop/pictortelescope/"+filename1, 'rb')
         
         p1 = MIMEBase('application', 'octet-stream')
         p1.set_payload((attachment).read())
@@ -187,7 +188,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
 
         #Add spectrum Data
         filename2 = 'data_spectrum.csv'
-        attachment2 = open("/home/pictor/Desktop/pictortelescope/"+filename2, 'rb')
+        attachment2 = open("/home/pi/Desktop/pictortelescope/"+filename2, 'rb')
 
         p2 = MIMEBase('application', 'octet-stream')
         p2.set_payload((attachment2).read())
@@ -198,7 +199,7 @@ Your observation's averaged spectrum, dynamic spectrum (waterfall) and Power vs 
 
         #Add Time vs. Power Data
         filename3 = 'data_time_power.csv'
-        attachment3 = open("/home/pictor/Desktop/pictortelescope/"+filename3, 'rb')
+        attachment3 = open("/home/pi/Desktop/pictortelescope/"+filename3, 'rb')
 
         p3 = MIMEBase('application', 'octet-stream')
         p3.set_payload((attachment2).read())
