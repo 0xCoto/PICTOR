f0487149941a9b4a86718c34e93b7713659901a0
Rick Sanchez <116969974+0x7269636B@users.noreply.github.com>
2023-06-21 19:58:00 +0300

    Update obs_scheduler.py
    Selenium replaced by HTTP Post request
    Default values in time-to-observe parameters
    Improved documentation and help

--- obs_scheduler.py
+++ obs_scheduler.py
@@ -4,7 +4,7 @@
 '''
 
 '''
-                                            Parameters:
+                            Parameters:
 +------------------------------------------------------------------------------------------------------+
 |   Parameter  | Variable name	|               Use	                       | Accepted values           |
 +--------------+----------------+------------------------------------------+---------------------------+
@@ -16,6 +16,7 @@
 |      -du	   |    Duration	|   Duration of observation	               |    Up to 600              |
 |      -rd	   |    Raw Data	|   Sends .csv data from observation	   |    {0, 1}                 |
 |      -e	   |    Email	    |   The email to send the data	           |    Any existing email     |
++--------------+----------------+------------------------------------------+---------------------------+
 |      -d	   |    Day	        |   The day to execute the observation	   |    {01, … , 31}           |
 |      -hr	   |    Hour	    |   The hour to execute the observation	   |    {00, … , 23}           |
 |      -mn	   |    Minute	    |   The minute to execute the observation  |    {00, …, 59}            |
@@ -25,19 +26,13 @@
 
 '''
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+import requests
 from time import sleep
 from datetime import datetime
 import argparse
 import os
 
-PICTOR = "https://pictortelescope.com/observe.php"  #pictor url
+PICTOR = "https://pictortelescope.com/observe"  #pictor url
 
 '''
 To make continuous observations, the step (degrees from last observation) should be half the telsescope's beamwidth (~10 deg => step = 5 deg)
@@ -64,156 +59,87 @@ or about 20 minutes and 53 seconds.
 
 STEP = 21*60 #turn to seconds by doing minutes*60
 
+#init datetime
+dt = datetime.now()
+
 parser = argparse.ArgumentParser()
 
 #observation name parameter
-parser.add_argument('-n', metavar="name", type=str, required=True, help="observation name")
+parser.add_argument('-n', metavar="name", type=str, required=True, help="observation name: Any String")
 #cetner frequency parameter
-parser.add_argument('-cf', metavar="frequency", type=str, required=False, help="center frequency")
+parser.add_argument('-cf', metavar="frequency", type=str, required=False, help="center frequency: 1300-1700 (MHz)")
 #bandwidth parameter
-parser.add_argument('-bw', metavar="bandwidth", type=str, required=False, help="frequency range")
+parser.add_argument('-bw', metavar="bandwidth", type=str, required=False, help="frequency range: {500, 1, 2, 2.4,3.2}")
 #bins parameter
-parser.add_argument('-b', metavar="bins", type=str, required=False, help="duration of each sample")
+parser.add_argument('-b', metavar="bins", type=str, required=False, help="duration of each sample: {256, 512, 1024, 2048}")
 #channels parameter
-parser.add_argument('-ch', metavar="channels", type=str, required=False, help="data points in freq axis")
+parser.add_argument('-ch', metavar="channels", type=str, required=False, help="data points in freq axis: Up to 20000")
 #duration parameter
-parser.add_argument('-du', metavar="duration", type=str, required=True, help="duration of observation")
+parser.add_argument('-du', metavar="duration", type=str, required=True, help="duration of observation: Up to 600  ")
 #raw data parameter
-parser.add_argument('-rd', metavar="data", type=str, required=False, help="raw data")
+parser.add_argument('-rd', metavar="data", type=str, required=False, help="raw data: {0, 1} ")
 #email parameter
-parser.add_argument('-e', metavar="email", type=str, required=True, help="email address to deliver the data")
+parser.add_argument('-e', metavar="email", type=str, required=True, help="email address to deliver the data: Any existing email ")
 #day parameter
-parser.add_argument('-d', metavar="day", type=int, required=False, help="day to execute observation")
+parser.add_argument('-d', metavar="day", type=int, required=False, help="day to execute observation: {01, … , 31} ", default=dt.day)
 #hour parameter
-parser.add_argument('-hr', metavar="hour", type=int, required=False, help="hour to execute observation")
+parser.add_argument('-hr', metavar="hour", type=int, required=False, help="hour to execute observation: {00, … , 23}", default=dt.hour)
 #minute parameter
-parser.add_argument('-mn', metavar="minute", type=int, required=False, help="minute to execute observation")
+parser.add_argument('-mn', metavar="minute", type=int, required=False, help="minute to execute observation: {00, …, 59} ", default=dt.minute)
 #repeat times parameter
-parser.add_argument('-rt', metavar="repeat", type=int, required=False, help="times to repeat observaton", default=1)
+parser.add_argument('-rt', metavar="repeat", type=int, required=False, help="times to repeat observaton: An integer ", default=1)
 #interval parameter
-parser.add_argument('-i', metavar="interval", type=int, required=False, help="time between observations", default=STEP)
+parser.add_argument('-i', metavar="interval", type=int, required=False, help="time between observations: An integer ", default=STEP)
 
 args = parser.parse_args()
 
-options = Options()
-
-#options to make webdriver silent
-os.environ['WDM_LOG_LEVEL'] = '0'  # Disable ChromeDriver logs
-options.add_argument("--headless")
-options.add_argument("--disable-logging")
-options.add_argument("--disable-infobars")
-options.add_argument("--disable-notifications")
-options.add_argument("--log-level=OFF")  # Disable Chrome browser logs
-options.add_argument("service_log_path='NUL'")  # Redirect output to /dev/null
-
 def send_data(name='', freq='', bandwidth='', bins='', channels='', duration='', raw='', email=''):
-    # Create a new instance of the Chrome driver
-    driver = webdriver.Chrome(options=options)
-
-    # Open the desired webpage
-    driver.get(PICTOR)
-
-    # Find the form elements and populate them with data
-    ####################################################
-
-    #observation name OK
-    input_field = driver.find_element(By.ID,'obs_name')
-    input_field.send_keys(name)
-
-    #center frequency OK
-    input_field = driver.find_element(By.ID,'f_center')
-    input_field.clear()
-    input_field.send_keys(freq)
-
-    #bandwidth 
-    input_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "bandwidth")))
-    dd = Select(input_field)
-    index = 0
-    if bandwidth == "500":#500kHz
-        index = 0
-    if bandwidth == "1":#1MHz
-        index = 1
-    if bandwidth == "2":#2Mhz
-        index = 2
-    if bandwidth == "2.4":#2.4MHz
-        index = 3
-    if bandwidth == "3.2":#3.2MHz
-        index = 4
-    #fix index
-    dd.select_by_index(index)
-
-    #channels
-    input_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "channels")))
-    dd = Select(input_field)
-    index = 0
-    if channels == 256:
-        index = 0
-    if channels == 512:
-        index = 1
-    if channels == 1024:
-        index = 3
-    if channels == 2048:
-        index = 4
-    #fix index
-    dd.select_by_index(index)
-
-    #bins
-    input_field = driver.find_element(By.ID,'nbins')
-    input_field.clear()
-    input_field.send_keys(bins)
-
-    #duration
-    input_field = driver.find_element(By.ID,'duration')
-    input_field.send_keys(duration)
-
-    #email
-    input_field = driver.find_element(By.ID,'email')
-    input_field.send_keys(email)
-
-    #raw data
-    input_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "raw_data")))
-    dd = Select(input_field)
-    index = 0
-    if raw == '1':
-        index = 1
-    #fix index
-    dd.select_by_index(index)
-
-    # Submit the form
-    submit_button = driver.find_element(By.NAME,'submit_btn')
-    submit_button.click()
-
-    # Close the browser
-    driver.quit()
-
-#init datetime
-dt = datetime.now()
+    '''
+    Create payload and send HTTP Post request
+    '''
+    payload = {
+        'obs_name': name,
+        'f_center': freq, 
+        'bandwidth': bandwidth+'mhz', 
+        'channels': channels, 
+        'nbins': bins, 
+        'duration': duration, 
+        'email': email, 
+        'submit_btn': '1'
+        }
+    
+    requests.post(PICTOR, payload)
 
 #set target date and time
 target_dt = datetime(dt.year, dt.month, args.d, args.hr, args.mn)#target datetime year, month, day, hours, minutes
 
+print()
+print("Waiting....")
+
 #get current date and time
 current_dt = datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute) 
-
-print("Waiting....")
 
 #wait until time has come
 while target_dt != current_dt:
     dt = datetime.now()
-    current_dt = current_dt = datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute)
+    current_dt = datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute)
     sleep(1)
 
 print("Starting....")
 for i in range (0, args.rt):
 
-    name = args.n+'_'+str(i+1)#rename observation according to iteration
-    print(name)
+    name = args.n+'_'+str(i+1)#create name
+    
+    #call send_data function
     send_data(name=name, freq=args.cf, bandwidth=args.bw, bins=args.b, channels=args.ch, duration=args.du, raw=args.rd, email=args.e)
+
+    #print the progress
     print("Done interation: ", i+1, "of:", args.rt, ",", ((i+1)/args.rt)*100, "%")
 
     #wait for the next iteration
-    if (args.rt > 1 and i != args.rt):
+    if (args.rt > 1 or i == args.rt):
         sleep(args.i)
 
+#print ok message and exit
 print("Data obtained")
 exit(0)
